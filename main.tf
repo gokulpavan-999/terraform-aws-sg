c4c167ababad54e601643603d4807f25f3a34b6b
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-30 11:23:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+resource "aws-security_group" "main" {
+  name = var.sg_name
+  description = var.sg_description
+  vpc_id = var.vpc_id
 
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-${var.sg_name}" #roboshop-dev-catalogue
+    }
+  )
+}
